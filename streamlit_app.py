50a542289fdbbb4d8354064eb6b35e96340d225d
chiangk-wp24-cell <chiangk-wp24@student.tarc.edu.my>
2026-04-14 03:02:05 +0800

    model_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,35 @@
 import streamlit as st
+import joblib import load
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+rf_model = joblib.load("rf_model.pkl")
+dt_model = joblib.load("dt_model.pkl")
+knn_model = joblib.load("knn_model.pkl")
+nb_model = joblib.load("nb_model.pkl")
+
+st.title("Obesity Level Prediction")
+
+age = st.number_input("Age")
+weight = st.number_input("Weight")
+height = st.number_input("Height")
+
+input_data = np.array([[age, weight, height]])
+
+model_choice = st.selectbox(
+    "Choose Model",
+    ["Random Forest", "Decision Tree", "KNN", "Logistic Regression"]
 )
+
+if model_choice == "Random Forest":
+    model = rf_model
+elif model_choice == "Decision Tree":
+    model = dt_model
+elif model_choice == "KNN":
+    model = knn_model
+else:
+    model = lr_model
+
+if st.button("Predict"):
+    prediction = model.predict(input_data)
+    st.success(f"Predicted Obesity Level: {prediction[0]}")
